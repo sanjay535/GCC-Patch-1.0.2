1616e269c322fefd27c0805023eb0277bc1df904
sanjay gautam <34466733+sanjay535@users.noreply.github.com>
2019-10-05 02:33:35 +0530

    Update Question6.java
    first commit

--- java/src/main/java/skeleton/answers/Question6.java
+++ java/src/main/java/skeleton/answers/Question6.java
@@ -1,9 +1,21 @@
 package skeleton.answers;
 
 public class Question6 {
-
+  static int yourMethod(char start, char end, String s[])
+    {
+        int idx=-1;
+        for(int i=s.length-1;i>=0;i++)
+        {
+          if(s[i].charAt(0)==start && s[i].charAt(s[i].length()-1)==end)
+          {
+              idx=i;
+              break;
+          }
+        }
+        return idx;
+    }
     public static int startingAndEndingWell(String[] portfolio) {
-        // TODO Auto-generated method stub
+        System.out.println(yourMethod('A', 'W', portfolio));
         return -5;
     }
 }

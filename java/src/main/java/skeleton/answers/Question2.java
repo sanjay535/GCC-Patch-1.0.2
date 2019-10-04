6ef865f8e070dcd716b038e58c1ef78a324facdf
sanjay gautam <34466733+sanjay535@users.noreply.github.com>
2019-10-05 03:23:52 +0530

    2nd question
    first commit

--- java/src/main/java/skeleton/answers/Question2.java
+++ java/src/main/java/skeleton/answers/Question2.java
@@ -3,7 +3,24 @@ package skeleton.answers;
 public class Question2 {
 
     public static int riskAndReward(int[] risk, int[] bonus, int[] trader) {
-        // TODO Auto-generated method stub
-        return -5;
+        
+        int sum=0;
+
+        for(int i=0;i<trader.length;i++)
+        {
+            int max=Integer.MIN_VALUE;
+           for(int j=0;j<risk.length;j++)
+           {
+               if(trader[i]>=risk[j])
+               {
+                 if(bonus[j]>max)
+                     max=bonus[j];
+
+               }
+           }
+           sum+=max;
+        }
+
+        return sum;
     }
 }
